f865cd56f89abcedf925ee6d31e42b6de06f80bb
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2025-02-07 13:18:56 +0300

    feat: create project (#1)
    * feat: create poject
    
    * feat: add main page and styles
    
    * feat: add company logo
    
    * feat: add ceilings images
    
    * feat: add header
    
    * feat: add chat and phone
    
    * feat: add swiper
    
    * refactor: change title

---
+++ scripts/swiper/main-swiper.js
@@ -0,0 +1,23 @@
+const swiper = new Swiper('.swiper', {
+    direction: 'horizontal',
+    loop: true,
+  
+    autoplay: {
+      delay: 10000,
+    },
+  
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
+  
