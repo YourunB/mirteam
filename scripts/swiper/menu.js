b44f54585ac3b5c6b218a133651c5e0cf2187a0c
yourunb <bxyura@gmail.com>
2025-02-10 22:45:38 +0300

    feat: block scroll on open menu

--- scripts/swiper/menu.js
+++ scripts/swiper/menu.js
@@ -3,6 +3,7 @@ const btnBurger = document.getElementById('btn-burger');
 const header = document.getElementById('header');
 
 btnBurger.addEventListener('click', () => {
+  document.body.classList.toggle('scroll-off')
   btnBurger.classList.toggle('burger_close');
   menu.classList.toggle('navigation_show');
   header.classList.toggle('header_active');
