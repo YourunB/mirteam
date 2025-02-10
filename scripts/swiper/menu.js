947847fb1ea545d706bf441b321e3f52bc4d50ba
yourunb <bxyura@gmail.com>
2025-02-10 22:05:27 +0300

    feat: add custom burger btn

---
+++ scripts/swiper/menu.js
@@ -0,0 +1,6 @@
+const menu = document.getElementById('navigation');
+const btnBurger = document.getElementById('btn-burger');
+
+btnBurger.addEventListener('click', () => {
+  btnBurger.classList.add('burger_close')
+})
