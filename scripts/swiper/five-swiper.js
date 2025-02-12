d610639f339aab5dcac677ebbe1febef96d25f31
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2025-02-12 17:26:08 +0300

    feat: add multiple slider (#9)
    * feat: add multiple slider
    
    * feat: add fancy box to multiple slider
    
    * feat: add zoom for multiple slider
    
    * refactor: change style to main section
    
    * refactor: change section style
    
    * refactor: add padding to section

---
+++ scripts/swiper/five-swiper.js
@@ -0,0 +1,27 @@
+const swiperFive = new Swiper('.swiper-container', {
+    spaceBetween: 10,
+    freeMode: false,
+    loop: true,
+    grabCursor: true,
+    navigation: {
+      prevEl: '.bottom-slider-button-prev',
+      nextEl: '.bottom-slider-button-next',
+    },
+    breakpoints: {
+      1200: {
+        slidesPerView: 5,
+      },
+      992: {
+        slidesPerView: 4,
+      },
+      768: {
+        slidesPerView: 3,
+      },
+      576: {
+        slidesPerView: 2,
+      },
+      0: {
+        slidesPerView: 1,
+      },
+    }
+  });
