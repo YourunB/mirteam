cc9c11fc4fe2e901686d518e2b1e4df84ee2713b
Yury Butskevich <124175026+YourunB@users.noreply.github.com>
2025-02-13 09:55:38 +0300

    feat: fill home page (#10)
    * feat: add preveiw content
    
    * feat: add company advantages
    
    * feat: add animation for rotate card
    
    * feat: add adaptive for mobile
    
    * feat: add services
    
    * feat: add promo and animation
    
    * refactor: delete padding from menu
    
    * feat: add prettier for project
    
    * feat: add prettier config
    
    * refactor: formate code with prettier

--- scripts/menu.js
+++ scripts/menu.js
@@ -3,14 +3,14 @@ const btnBurger = document.getElementById('btn-burger');
 const header = document.getElementById('header');
 
 btnBurger.addEventListener('click', () => {
-  document.body.classList.toggle('scroll-off')
+  document.body.classList.toggle('scroll-off');
   btnBurger.classList.toggle('burger_close');
   menu.classList.toggle('navigation_show');
   header.classList.toggle('header_active');
-})
+});
 
 window.addEventListener('scroll', () => {
-  console.log('aa')
-  if (document.body.getBoundingClientRect().y < 0) header.classList.add('header_sticky')
-  else header.classList.remove('header_sticky')
+  console.log('aa');
+  if (document.body.getBoundingClientRect().y < 0) header.classList.add('header_sticky');
+  else header.classList.remove('header_sticky');
 });
--- scripts/swiper/five-swiper.js
+++ scripts/swiper/five-swiper.js
@@ -1,27 +1,27 @@
 const swiperFive = new Swiper('.swiper-container', {
-    spaceBetween: 10,
-    freeMode: false,
-    loop: true,
-    grabCursor: true,
-    navigation: {
-      prevEl: '.bottom-slider-button-prev',
-      nextEl: '.bottom-slider-button-next',
+  spaceBetween: 10,
+  freeMode: false,
+  loop: true,
+  grabCursor: true,
+  navigation: {
+    prevEl: '.bottom-slider-button-prev',
+    nextEl: '.bottom-slider-button-next',
+  },
+  breakpoints: {
+    1200: {
+      slidesPerView: 5,
     },
-    breakpoints: {
-      1200: {
-        slidesPerView: 5,
-      },
-      992: {
-        slidesPerView: 4,
-      },
-      768: {
-        slidesPerView: 3,
-      },
-      576: {
-        slidesPerView: 2,
-      },
-      0: {
-        slidesPerView: 1,
-      },
-    }
-  });
+    992: {
+      slidesPerView: 4,
+    },
+    768: {
+      slidesPerView: 3,
+    },
+    576: {
+      slidesPerView: 2,
+    },
+    0: {
+      slidesPerView: 1,
+    },
+  },
+});
