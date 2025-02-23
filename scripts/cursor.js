1a6d146121bf0b2252de364b6008db621a3a4332
yourunb <bxyura@gmail.com>
2025-02-23 12:27:16 +0300

    refactor: update cursor script

--- scripts/cursor.js
+++ scripts/cursor.js
@@ -1,13 +1,12 @@
 const cursor = document.createElement('div');
-cursor.classList.add('cursor');
+cursor.className = 'cursor unvisible';
 document.body.prepend(cursor);
 
 document.addEventListener('mousemove', (e) => {
   if (navigator.maxTouchPoints > 0) {
     cursor.classList.add('unvisible');
-    return;
-  }
-  if (navigator.maxTouchPoints === 0) {
+  } else {
+    cursor.classList.remove('unvisible');
     const x = e.clientX;
     const y = e.clientY;
     cursor.style.transform = `translate(${x - 10}px, ${y - 10}px)`;
